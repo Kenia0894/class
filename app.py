40f5656f5991a70fa1b5d250955cd6827019e53e
Kenia0894 <120056540+Kenia0894@users.noreply.github.com>
2023-11-29 19:51:15 -0500

    Add files via upload
    clase nueva

---
+++ app.py
@@ -0,0 +1,58 @@
+from flask import Flask ,render_template
+import requests
+from dotenv import load_dotenv,dotenv_values
+from sqlalchemy import create_engine, Integer, MetaData, Table, Column, String
+
+metaData=MetaData
+
+#definicion de tablas
+cities= Table ('cities',metaData,
+               Column('id',Integer (), primary_key=True, autoincrement=True),
+               Column('id',String(100),Nullable=True, unique=True) 
+               )
+
+
+config = dotenv_values('.env')
+
+app = Flask (__name__)
+
+engine =create_engine("sqlite:///weather.db")
+
+def get_weather_data(city):
+     API_KEY = config['API_KEY']
+     url= f'https://api.openweathermap.org/data/2.5/weather?q={ city }&appid={API_KEY}&lang=es&units=metric'
+     r= requests.get(url).json()
+     print(r)
+     return r
+
+@app.route('/KENNYA')
+def KENNYA():
+   return get_weather_data('Guayaquil')
+
+  
+
+@app.route('/about')
+def about():
+   return render_template('curriculum.html')
+
+@app.route('/clima')
+def clima():
+   return 'clima'
+
+@app.route('/PRUEBA')
+def PRUEBA():
+   clima =get_weather_data('Quito')
+   temperatura =str(clima['main']['temp'])
+   descripcion= str(clima['weather'][0]['description'])
+   icono = str(clima['weather'][0]['icon'])
+
+   r_json={
+        'ciudad': 'Quito',
+        'temperatura':temperatura,
+        'descripcion' :descripcion,
+        'icono':icono
+   }
+   return render_template('weather.html',clima = r_json)
+   
+if __name__ == '__main__':
+    app.run(debug=True)
